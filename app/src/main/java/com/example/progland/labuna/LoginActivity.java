a54d75661bc4b2477d8972017220b267ff0102dc
CIUNACOTO <deejaymarlon506@hotmail.es>
2018-03-07 21:58:41 -0600

    actividad menu

--- app/src/main/java/com/example/progland/labuna/LoginActivity.java
+++ app/src/main/java/com/example/progland/labuna/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.progland.labuna;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -88,6 +89,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intento = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivity(intento);
                 attemptLogin();
             }
         });
