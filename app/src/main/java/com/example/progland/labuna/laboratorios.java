af1386a7fb0ed75990a0c4d6744fd4e2feb81ef4
Derian37 <32728553+Derian37@users.noreply.github.com>
2018-06-12 01:13:39 -0600

    d

--- app/src/main/java/com/example/progland/labuna/laboratorios.java
+++ app/src/main/java/com/example/progland/labuna/laboratorios.java
@@ -3,36 +3,67 @@
  */
 package com.example.progland.labuna;
 
-//clase para guardar y optener los laboratorios
+
+/**
+ * clase para guardar y optener los laboratorios
+ */
 public class laboratorios {
     private  int lid;//id del lab
     private  String nombre;//nombre del lab
 
-    public  laboratorios(){//constructor sin parametros
+    /**
+     * constructor sin parametros
+     */
+    public  laboratorios(){
 
     }
-    public  laboratorios(int lid, String nombre){//constructor parametrisado
+
+    /**
+     * Constructor parametrisado
+     * @param lid Parametro tipo Int
+     * @param nombre Parametro tipo String
+     */
+    public  laboratorios(int lid, String nombre){
         this.lid=lid;
         this.setNombre(nombre);
     }
 
-//get y set del id
+    /**
+     * Funcion get Id, extrae ids guardados en la clase.
+     * @return
+     */
     public int getId() {
         return this.lid;
     }
+
+    /**
+     * Metodo SetNombre, aplica o guarda un id
+     * @param id Parametro tipo int
+     */
     public void setId(int id) {
         this.lid = lid;
     }
-//get y set del nomre
+
+    /**
+     * Metodo SetNombre, aplica o guarda un nombre
+     * @param nombre Parametro tipo String
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    /**
+     * Funcion getNombre, extrae Nombres guardados en la clase.
+     * @return
+     */
     public String getNombre() {
         return this.nombre;
     }
 
-
-//metodo para devolver el nombre
+     /**
+     * Funcion  para devolver el nombre
+     * @return
+     */
     @Override
     public String toString(){
         return nombre;
