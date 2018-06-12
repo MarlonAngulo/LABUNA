f2882382ddaefa94fd39b23794b5e02e77dffd7e
CIUNACOTO <deejaymarlon506@hotmail.es>
2018-06-12 01:31:08 -0600

    mensaje de usuario incorrecto

--- app/src/main/java/com/example/progland/labuna/LoginActivity.java
+++ app/src/main/java/com/example/progland/labuna/LoginActivity.java
@@ -256,6 +256,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             {
                 Mensaje("usuario incorrecto");
             }
+            finish();
+            break;
 
 
         }
